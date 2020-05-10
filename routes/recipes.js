995a5747e8e2c39aaf2a1505df073a2afbf4bb40
cardotisler <cardotisler@live.com>
2020-05-10 15:53:49 +0300

    heroku

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,7 +6,7 @@ var middleware = require("../middleware/index.js")
 // <------------ RECIPES ROUTES ------------>
 //index page
 router.get("/", function(req, res){
-    res.sendFile('index.html', {root: "."});
+    res.render("landing");
 });
 
 //INDEX route
