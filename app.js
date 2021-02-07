0f36bab55c65d5315a4e0f9ceca200a442e366cb
Cardo <cardotisler@live.com>
2021-02-07 19:18:32 +0200

    port fix

--- app.js
+++ app.js
@@ -62,6 +62,6 @@ app.use(commentsRoutes);
 app.use(recipesRoutes);
 app.use(indexRoutes);
 
-app.listen(3000, process.env.IP, function(){
-    console.log("Server running on port 3000.");
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server running on port ", process.env.PORT);
 });
